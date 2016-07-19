01c0b6ddbd33a4ae77fbd56d80c257ca8d25d62f
CBarros <carla.88.barros@gmail.com>
2016-07-19 14:53:38 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,16 @@
+# EEG_Preprocessing
+
+% Test different digital filters effects on continuous and discrete wavelet application
+% Using EEG data from an oddball task
+% 
+% Carla Barros - University of Minho 2016
+
+%{
+
+Non-filtered
+
+4th order Butterworth filter
+
+FIRFilt - Kaiser windowed
+
+%}
